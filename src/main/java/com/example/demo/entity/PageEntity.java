b98c862f69a576c62365f9aff2ace606bdf5294c
Your Name <you@example.com>
2023-03-03 17:13:08 +0800

    11

--- src/main/java/com/example/demo/entity/PageEntity.java
+++ src/main/java/com/example/demo/entity/PageEntity.java
@@ -1,13 +1,14 @@
 package com.example.demo.entity;
 
 
+import com.baomidou.mybatisplus.annotation.TableField;
 import io.swagger.annotations.ApiModelProperty;
 
 public class PageEntity {
-
+    @TableField(exist = false)
     @ApiModelProperty(value = "当前页")
     private Integer pageNumber = 0;
-
+    @TableField(exist = false)
     @ApiModelProperty(value = "每页数据量")
     private Integer pageSize = 10;
 
