2d6ff7a9b2e24b41b65d9f474897501e2d6f484f
Your Name <you@example.com>
2023-03-09 11:18:39 +0800

    1345

--- src/main/java/com/example/demo/controller/PmController.java
+++ src/main/java/com/example/demo/controller/PmController.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.example.demo.controller;
 
 
@@ -67,74 +66,3 @@ public class PmController {
         return AjaxResult.success(PmService.getAll(PmEntity));
     }
 }
-=======
-package com.example.demo.controller;
-
-
-import com.example.demo.entity.PmEntity;
-import com.example.demo.server.PmService;
-import com.example.demo.web.AjaxResult;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-/**
- * <p>
- *  前端控制器
- * </p>
- *
- * @author liuxiaobao
- * @since 2023-03-01
- */
-@RestController
-@CrossOrigin
-@RequestMapping("/pm-entity")
-public class PmController {
-    @Autowired
-    private PmService PmService;
-    /**
-     * 新增汽车信息
-     * @param PmEntity
-     * @return
-     */
-    @PostMapping("/save")
-    public AjaxResult savePm(@RequestBody PmEntity PmEntity){
-        PmService.save(PmEntity);
-        return AjaxResult.success("新增汽车信息成功");
-    }
-
-    /**
-     * 批量删除汽车信息
-     * @param PmIds
-     * @return
-     */
-    @PostMapping("/delete")
-    public AjaxResult deletePm(@RequestBody List<Integer> PmIds){
-        PmService.removeByIds(PmIds);
-        return AjaxResult.success("删除汽车信息成功");
-    }
-
-    /**
-     * 修改汽车信息
-     * @param PmEntity
-     * @return
-     */
-    @PostMapping("/update")
-    public AjaxResult updatePm(@RequestBody PmEntity PmEntity){
-        PmService.updateById(PmEntity);
-        return AjaxResult.success("修改汽车信息成功");
-    }
-
-    /**
-     * 查询汽车成功
-     * @param PmEntity
-     * @return
-     */
-    @PostMapping("/select")
-    public AjaxResult selectPm(@RequestBody PmEntity PmEntity){
-        List<PmEntity> list = PmService.getAll(PmEntity);
-        return AjaxResult.success("修改汽车信息成功");
-    }
-}
->>>>>>> 4d743afdae11cf4a101037e57113edd76d3f7080
--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.example.demo.controller;
 
 
@@ -67,74 +66,3 @@ public class UserController {
         return AjaxResult.success(UserService.getAll(UserEntity));
     }
 }
-=======
-package com.example.demo.controller;
-
-
-import com.example.demo.entity.UserEntity;
-import com.example.demo.server.UserService;
-import com.example.demo.web.AjaxResult;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-/**
- * <p>
- *  前端控制器
- * </p>
- *
- * @author liuxiaobao
- * @since 2023-03-01
- */
-@RestController
-@CrossOrigin
-@RequestMapping("/user-entity")
-public class UserController {
-    @Autowired
-    private UserService UserService;
-    /**
-     * 新增汽车信息
-     * @param UserEntity
-     * @return
-     */
-    @PostMapping("/save")
-    public AjaxResult saveUser(@RequestBody UserEntity UserEntity){
-        UserService.save(UserEntity);
-        return AjaxResult.success("新增汽车信息成功");
-    }
-
-    /**
-     * 批量删除汽车信息
-     * @param UserIds
-     * @return
-     */
-    @PostMapping("/delete")
-    public AjaxResult deleteUser(@RequestBody List<Integer> UserIds){
-        UserService.removeByIds(UserIds);
-        return AjaxResult.success("删除汽车信息成功");
-    }
-
-    /**
-     * 修改汽车信息
-     * @param UserEntity
-     * @return
-     */
-    @PostMapping("/update")
-    public AjaxResult updateUser(@RequestBody UserEntity UserEntity){
-        UserService.updateById(UserEntity);
-        return AjaxResult.success("修改汽车信息成功");
-    }
-
-    /**
-     * 查询汽车成功
-     * @param UserEntity
-     * @return
-     */
-    @PostMapping("/select")
-    public AjaxResult selectUser(@RequestBody UserEntity UserEntity){
-        List<UserEntity> list = UserService.getAll(UserEntity);
-        return AjaxResult.success("修改汽车信息成功");
-    }
-}
->>>>>>> 4d743afdae11cf4a101037e57113edd76d3f7080
